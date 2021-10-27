317f468330a247f3e81661c53df36acbf3d34873
lijinli <lijinli@nullmax.ai>
2021-10-27 10:25:01 +0800

    null

--- emulate_cost_script.sh
+++ emulate_cost_script.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
-zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 500 data1.csv big_cost" &
-zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 500 data2.csv small_cost" &
+zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 1000000 10-27_big_data1.csv big_cost" &
+zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 1000000 10-27_big_data2.csv big_cost" &
+zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 1000000 10-27_big_data3.csv big_cost" &
+zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 1000000 10-27_big_data4.csv big_cost" &
+zsh -c "source ~/.zshrc; conda activate torch; python emulate_cost.py 1000000 10-27_small_data5.csv small_cost" &
 
