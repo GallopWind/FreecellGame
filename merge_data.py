b1713d87282afc7fc42c63640ac567ec1420bd50
NULL <NULL>
2021-10-28 00:04:14 +0800

    null

--- merge_data.py
+++ merge_data.py
@@ -17,6 +17,6 @@ if __name__ == '__main__':
     for bag_name in data_bags_name:
         data = pd.read_csv('data/' + bag_name, index_col=0)
         data_frames.append(data)
-    concat_data = pd.concat(data_frames)
+    concat_data = pd.concat(data_frames, axis=0)
     merge_data = concat_data.groupby(concat_data.index).apply(select_lowest_cost)
     merge_data.to_csv('data/' + output_data_bag_name)
