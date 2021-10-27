196bab640362ecd3de863f52e7a0a539d1382892
lijinli <lijinli@nullmax.ai>
2021-10-27 14:25:12 +0800

    null

---
+++ merge_data.py
@@ -0,0 +1,22 @@
+import pandas
+import sys
+
+import pandas as pd
+
+
+def select_lowest_cost(dataframe):
+    cost = dataframe.loc[:, 'cost']
+    index = cost.argmin(axis=0)
+    return dataframe.iloc[index, :]
+
+
+if __name__ == '__main__':
+    data_bags_name = sys.argv[1:-1]
+    output_data_bag_name = sys.argv[-1]
+    data_frames = []
+    for bag_name in data_bags_name:
+        data = pd.read_csv('data/' + bag_name, index_col=0)
+        data_frames.append(data)
+    concat_data = pd.concat(data_frames)
+    merge_data = concat_data.groupby(concat_data.index).apply(select_lowest_cost)
+    merge_data.to_csv('data/' + output_data_bag_name)
--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-def func1():
-    return 1, 2
+import pandas
+import pandas as pd
+import numpy as np
 
 
-def func2(a, b):
-    print(a, b)
+def get_lowest_cost(dataframe):
+    cost = dataframe.loc[:, 'cost']
+    index = cost.argmin(axis=0)
+    return dataframe.iloc[index, :]
 
 
-func2(*func1())
+if __name__ == '__main__':
+    data1 = pd.DataFrame(columns=['id', 'cost'], dtype='int')
+    data2 = pd.DataFrame(columns=['id', 'cost'], dtype='int')
+    data1.loc['hash1'] = [1, 10]
+    data1.loc['hash2'] = [2, 12]
+    data1.loc['hash3'] = [3, 9]
+    data2.loc['hash1'] = [1, 7]
+    data2.loc['hash2'] = [2, 14]
+    data2.loc['hash3'] = [3, 9]
+    data1.to_csv('data/data1.csv')
+    data2.to_csv('data/data2.csv')
+
+    data0 = pd.concat([data1, data2])
+    group_data = data0.groupby(data0.index)
+
+    merge_data = group_data.apply(get_lowest_cost)
+    group_data
