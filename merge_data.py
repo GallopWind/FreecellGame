3c11007b88b04c0e9af7a51725bea76c0338f15b
lijinli <lijinli@nullmax.ai>
2021-10-28 12:01:24 +0800

    null

--- merge_data.py
+++ merge_data.py
@@ -3,6 +3,17 @@ import sys
 
 import pandas as pd
 
+NAMES = ['primary_key']
+for x in range(52):
+    NAMES.append(('%02d' % x) + 'id')
+    NAMES.append(('%02d' % x) + 'index')
+NAMES.append('cost')
+
+DTYPES = dict()
+DTYPES[NAMES[0]] = 'str'
+for x in range(1, 106):
+    DTYPES[NAMES[x]] = 'uint8'
+
 
 def select_lowest_cost(dataframe):
     cost = dataframe.loc[:, 'cost']
@@ -11,11 +22,14 @@ def select_lowest_cost(dataframe):
 
 
 if __name__ == '__main__':
+    '''
+        Usage: cmd data1 data2 data_out
+    '''
     data_bags_name = sys.argv[1:-1]
     output_data_bag_name = sys.argv[-1]
     data_frames = []
     for bag_name in data_bags_name:
-        data = pd.read_csv('data/' + bag_name, index_col=0)
+        data = pd.read_csv('data/' + bag_name, index_col=0, dtype=DTYPES)
         data_frames.append(data)
     concat_data = pd.concat(data_frames, axis=0)
     merge_data = concat_data.groupby(concat_data.index).apply(select_lowest_cost)
